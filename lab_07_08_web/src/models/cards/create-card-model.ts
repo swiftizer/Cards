64dda996e049448b91a0e9ba1c4e814f79061bbb
Sergey Nikolaev <ser.nikolaev@vk.team>
2023-12-14 19:29:18 +0300

    Lab 09

--- lab_07_08_web/src/models/cards/create-card-model.ts
+++ lab_07_08_web/src/models/cards/create-card-model.ts
@@ -24,7 +24,7 @@ export const useCreateCardModel = (apiService: CardService, { cardSetId, onSucce
 
             onSuccess?.();
         } catch (err) {
-            console.log("Ошибка при создании набора карточек");
+            console.log("");
         }
     };
 
